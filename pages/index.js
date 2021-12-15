6914cbc0101fd03c6d3c466df2b222bfb0a14c35
Yana S <71519783+StruninaYana98@users.noreply.github.com>
2021-12-15 12:10:58 +0300

    nextjs app created

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Takeda Oncology</title>
+        <meta name="description" content="Takeda Oncology Site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </div>
+  );
+}
