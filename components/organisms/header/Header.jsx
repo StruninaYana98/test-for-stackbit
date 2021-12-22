9c128d188a41ad60c88c5f219111116dbdefc8f1
Stackbit Code Editor <projects@stackbit.com>
2021-12-22 09:23:09 +0000

    Header.jsx: changed

--- components/organisms/header/Header.jsx
+++ components/organisms/header/Header.jsx
@@ -1,58 +1,57 @@
+import React, { useState } from "react";
 import Icon from "../../atoms/icons/Icon";
 import SvgLogo from "../../atoms/icons/SvgLogo";
+import Search from "../../molecules/search/Search";
 import EyebrowMenu from "../menu/EyebrowMenu";
 import MainMenu from "../menu/MainMenu";
 import MobileMenu from "../menu/MobileMenu";
 
-export default function Header() {
-  const menuItems = [
-    {
-      isActive: true,
-      subject: {
-        name: "MenuItem1",
-        href: "#",
-      }
-    },
-    {
-      subject: {
-        name: "MenuItem2",
-        href: "#",
-      }
-    },
-    {
-      subject: {
-        name: "MenuItem3",
-        href: "#",
-      }
-    },
-    {
-      subject: {
-        name: "MenuItem4",
-        href: "#",
-      }
-     
-    },
-  ];
-  const menuLinks=[
-    {label:"item1", href:"#"},{label:"item2", href:"#"}, {label:"item3", href:"#"}
-  ]
+export default function Header({
+  mainNavigation,
+  headerPrimaryLinks,
+  headerLogo,
+  headerSearch,
+}) {
+  const [mobileSearchOpen, setMobileSearchOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <header className="takui-c-header header-sticky">
       <div className="takui-c-desktop-view">
         <div className="takui-c-first-row">
-          <EyebrowMenu items={menuLinks}/>
+          <EyebrowMenu
+            items={headerPrimaryLinks}
+            logo={headerLogo}
+            search={headerSearch}
+          />
         </div>
         <div className="takui-c-second-row">
-          <MainMenu items={menuItems} />
+          <MainMenu items={mainNavigation} />
         </div>
       </div>
-      <div className="takui-c-mobile-view" id="mobile-menu">
+      <div
+        className={`takui-c-mobile-view ${mobileSearchOpen ? `search` : ``} ${
+          mobileMenuOpen ? `open` : ``
+        }`}
+        id="mobile-menu"
+      >
         <div className="takui-c-top-nav">
-          <div className="takui-c-logo"><SvgLogo/></div>
-          <div className="takui-c-icon-search"><Icon/></div>
+          <div className="takui-c-logo">
+            <SvgLogo
+              href={headerLogo.href}
+              target={headerLogo.target}
+              icon={headerLogo.icon}
+            />
+          </div>
+          <div
+            className="takui-c-icon-search"
+            onClick={() => setMobileSearchOpen(!mobileSearchOpen)}
+          >
+            <Icon icon={headerSearch.icon} />
+          </div>
           <div
             className="takui-ham-menu takui-c-hamburger"
-            data-menu="mobile-menu"
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
           >
             <div className="hamburger">
               <span></span>
@@ -63,9 +62,19 @@ export default function Header() {
         </div>
         <div className="takui-c-mobile-search-and-menu">
           <div className="takui-c-mobile-search">
-            {"{>molecules-search iconPath=mobileIcon}"}
+            <Search
+              label={null}
+              inputId={headerSearch.inputId}
+              placeholder={headerSearch.placeholder}
+              autoComplete={headerSearch.autoComplete}
+              icon={headerSearch.icon}
+            />
           </div>
-          <MobileMenu logo={{href:"#", text:"Oncology"}} items={menuItems} bottomLinks={menuLinks}/>
+          <MobileMenu
+            logo={{ href: "#", text: "Oncology" }}
+            items={mainNavigation}
+            bottomLinks={headerPrimaryLinks}
+          />
         </div>
       </div>
     </header>
