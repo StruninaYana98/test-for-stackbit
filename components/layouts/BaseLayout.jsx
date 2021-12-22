662febb6bdb7903c6db34d9ea90843cf34b393c1
Stackbit Code Editor <projects@stackbit.com>
2021-12-22 10:04:49 +0000

    BaseLayout.jsx: changed

--- components/layouts/BaseLayout.jsx
+++ components/layouts/BaseLayout.jsx
@@ -16,7 +16,17 @@ export default function BaseLayout({ site, page, children }) {
         headerSearch={site.headerSearch}
       />
       <main className="c-layout-main">
-        <div className="takui-c-site-width">{children}</div>
+        <div className="takui-c-site-width">{sections.length > 0 && (
+                    <div data-sb-field-path="sections">
+                        {sections.map((section, index) => {
+                            const Component = getComponent(section.type);
+                            if (!Component) {
+                                throw new Error(`no component matching the page section's type: ${section.type}`);
+                            }
+                            return <Component key={index} {...section} data-sb-field-path={`sections.${index}`} />;
+                        })}
+                    </div>
+                )}</div>
       </main>
       <Footer
         footerLogo={site.footerLogo}
