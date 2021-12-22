cd28e4aa4bfb53e5d097c881d91c8edd0d1e04c6
Stackbit Code Editor <projects@stackbit.com>
2021-12-22 09:24:47 +0000

    Dropdown.jsx: changed

--- components/molecules/dropdowns/Dropdown.jsx
+++ components/molecules/dropdowns/Dropdown.jsx
@@ -1,14 +1,16 @@
-export default function Dropdown({ subject, dropdownItems }) {
+import Link from "../../atoms/links/Link";
+
+export default function Dropdown({ text, href, dropdownItems }) {
   return (
-    <div class="takui-dropdown">
-      <div class="takui-dropdown-subject">
-        <a href={subject.href}>{subject.name}</a>
+    <div className="takui-dropdown">
+      <div className="takui-dropdown-subject">
+        <Link href={href}>{text}</Link>
       </div>
-      <div class="takui-dropdown-list-items">
+      <div className="takui-dropdown-list-items">
         {dropdownItems
           ? dropdownItems.map((item, index) => (
-              <div class="takui-dropdown-item" key={index}>
-                <a href={item.href}>{item.name}</a>
+              <div className="takui-dropdown-item" key={index}>         
+                <Link href={item.href} target={item.target}>{item.text}</Link>
               </div>
             ))
           : null}
