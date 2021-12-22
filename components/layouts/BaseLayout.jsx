e5e49ebc6f2f38a65115f4c9e98bd84b9f649241
Stackbit Code Editor <projects@stackbit.com>
2021-12-22 10:09:41 +0000

    BaseLayout.jsx: changed

--- components/layouts/BaseLayout.jsx
+++ components/layouts/BaseLayout.jsx
@@ -21,10 +21,7 @@ export default function BaseLayout({ site, page, children }) {
         {sections.length > 0 && (
                     <div data-sb-field-path="sections">
                         {sections.map((section, index) => {
-                            const Component = SectionBanner 
-                            if (!Component) {
-                                throw new Error(`no component matching the page section's type: ${section.type}`);
-                            }
+                            const Component = SectionBanner
                             return <Component key={index} {...section} data-sb-field-path={`sections.${index}`} />;
                         })}
                     </div>
